35369c58271682132bf4872b96c1b1c0fc529a36
sham77509 <67137191+sham77509@users.noreply.github.com>
2023-02-15 19:29:26 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -72,3 +72,22 @@ def random_select(labels):
 
 def qr_decomp(matrix):
 	pass
+def cluster_accuracy(y_true, y_pred):
+    """
+    Calculate clustering accuracy.
+    # Arguments
+        y: true labels, numpy.array with shape `(n_samples,)`
+        y_pred: predicted labels, numpy.array with shape `(n_samples,)`
+    # Return
+        accuracy, in [0,1]
+    """
+    y_true = y_true.astype(np.int64)
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+    # from sklearn.utils.linear_assignment_ import linear_assignment
+    from scipy.optimize import linear_sum_assignment as linear_assignment
+    ind_row, ind_col = linear_assignment(w.max() - w)
+    return sum([w[i, j] for i, j in zip(ind_row, ind_col)]) * 1.0 / y_pred.size
